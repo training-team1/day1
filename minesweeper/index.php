9650a5c773ebda6d2fb46d48c2d085fbcc71c25a
Agus Ariyanto <agusari@gmail.com>
2016-06-27 15:22:11 +0700

    Create index.php

---
+++ minesweeper/index.php
@@ -0,0 +1,83 @@
+<span id="title">Minesweeper</span>
+<table>
+<tr>
+	<td id="cell-1x1"></td>
+	<td id="cell-1x2"></td>
+	<td id="cell-1x3"></td>
+	<td id="cell-1x4"></td>
+	<td id="cell-1x5"></td>
+	<td id="cell-1x6"></td>
+	<td id="cell-1x7"></td>
+	<td id="cell-1x8"></td>
+</tr>
+<tr>
+	<td id="cell-2x1"></td>
+	<td id="cell-2x2"></td>
+	<td id="cell-2x3"></td>
+	<td id="cell-2x4"></td>
+	<td id="cell-2x5"></td>
+	<td id="cell-2x6"></td>
+	<td id="cell-2x7"></td>
+	<td id="cell-2x8"></td>
+</tr>
+<tr>
+	<td id="cell-3x1"></td>
+	<td id="cell-3x2"></td>
+	<td id="cell-3x3"></td>
+	<td id="cell-3x4"></td>
+	<td id="cell-3x5"></td>
+	<td id="cell-3x6"></td>
+	<td id="cell-3x7"></td>
+	<td id="cell-3x8"></td>
+</tr>
+<tr>
+	<td id="cell-4x1"></td>
+	<td id="cell-4x2"></td>
+	<td id="cell-4x3"></td>
+	<td id="cell-4x4"></td>
+	<td id="cell-4x5"></td>
+	<td id="cell-4x6"></td>
+	<td id="cell-4x7"></td>
+	<td id="cell-4x8"></td>
+</tr>
+<tr>
+	<td id="cell-5x1"></td>
+	<td id="cell-5x2"></td>
+	<td id="cell-5x3"></td>
+	<td id="cell-5x4"></td>
+	<td id="cell-5x5"></td>
+	<td id="cell-5x6"></td>
+	<td id="cell-5x7"></td>
+	<td id="cell-5x8"></td>
+</tr>
+<tr>
+	<td id="cell-6x1"></td>
+	<td id="cell-6x2"></td>
+	<td id="cell-6x3"></td>
+	<td id="cell-6x4"></td>
+	<td id="cell-6x5"></td>
+	<td id="cell-6x6"></td>
+	<td id="cell-6x7"></td>
+	<td id="cell-6x8"></td>
+</tr>
+<tr>
+	<td id="cell-7x1"></td>
+	<td id="cell-7x2"></td>
+	<td id="cell-7x3"></td>
+	<td id="cell-7x4"></td>
+	<td id="cell-7x5"></td>
+	<td id="cell-7x6"></td>
+	<td id="cell-7x7"></td>
+	<td id="cell-7x8"></td>
+</tr>
+<tr>
+	<td id="cell-8x1"></td>
+	<td id="cell-8x2"></td>
+	<td id="cell-8x3"></td>
+	<td id="cell-8x4"></td>
+	<td id="cell-8x5"></td>
+	<td id="cell-8x6"></td>
+	<td id="cell-8x7"></td>
+	<td id="cell-8x8"></td>
+</tr>
+</table>
